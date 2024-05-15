075a989d0083f7334faf04238cb4bbb3d0331228
Vinicius Orsi <vinicius.orsi@autopass.com.br>
2024-05-14 21:11:17 -0300

    alterando tela de login e adicionando barra lateral na home

--- ola_mundo/lib/login_page.dart
+++ ola_mundo/lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ola_mundo/home_page.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,64 +13,106 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 150,
+                height: 150,
+                // child: Image.asset('assets/imagens/logo.png'),
+                child: SvgPicture.asset(
+                  'assets/imagens/aviao.svg',
+                ),
+              ),
+              // ),
+              Container(height: 20),
+              Container(
+                height: 20,
+              ),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 12, right: 12, top: 20, bottom: 12),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        onChanged: (text) {
+                          password = text;
+                        },
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(190, 157, 38, 236)),
+                        child: const SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            'ENTRAR',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (email.toLowerCase() == 'teste@teste' &&
+                              password == '777') {
+                            Navigator.of(context).pushNamed('/home');
+                          } else {
+                            print('Login invalido');
+                          }
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  child: Image.network(
-                      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Logo_TV_2015.png/250px-Logo_TV_2015.png'),
-                ),
-                Container(
-                  height: 20,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  child: const Text('ENTRAR'),
-                  onPressed: () {
-                    if (email.toLowerCase() == 'teste@teste' &&
-                        password == '777') {
-                      Navigator.of(context).pushNamed('/home');
-                    } else {
-                      print('Login invalido');
-                    }
-                  },
-                )
-              ],
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/imagens/pista.jpg',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
+          Container(
+            color: Colors.black.withOpacity(0.1),
+          ),
+          _body(),
+        ],
       ),
     );
   }
