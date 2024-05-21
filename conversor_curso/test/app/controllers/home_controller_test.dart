2977980bbbc7595f8f6cc108b28e9cb86c6941f8
Vinicius Orsi <vinicius.orsi@autopass.com.br>
2024-05-21 15:55:49 -0300

    criando controller e plugando no dropdownbutton aula 35

---
+++ conversor_curso/test/app/controllers/home_controller_test.dart
@@ -0,0 +1,32 @@
+import 'package:conversor_curso/app/controllers/home_controller.dart';
+import 'package:conversor_curso/app/models/currency_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final TextEditingController toText = TextEditingController();
+  final TextEditingController fromText = TextEditingController();
+  final homeController = HomeController(toText, fromText);
+
+  test('Deve converter de real para dolar', () {
+    toText.text = '2.0';
+    homeController.convert();
+    expect(fromText.text, '0.36');
+  });
+
+  test('Deve converter de real para dolar com virgula', () {
+    toText.text = '2,0';
+    homeController.convert();
+    expect(fromText.text, '0.36');
+  });
+
+  test('Deve converter de dolar para real', () {
+    toText.text = '1.0';
+    homeController.toCurrency = CurrencyModel(
+        name: 'Dolar', real: 5.63, dolar: 1.0, euro: 0.85, bitcoin: 0.000088);
+    homeController.fromCurrency = CurrencyModel(
+        name: 'Real', real: 1.0, dolar: 0.18, euro: 0.15, bitcoin: 0.000016);
+    homeController.convert();
+    expect(fromText.text, '5.63');
+  });
+}
